b7d25a4ec6a4b4aba9dd9b53c890cf668a9d4d17
Ash Jain <aakashjainofficial@gmail.com>
2021-11-19 20:17:59 +0530

    Reactified navbar with states, improved UI.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,28 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import '../css/navbar.css';
 
 function Navbar() {
+    const [hamburger, setHamburger] = useState(false);
+    const toggleHamburger = () => { setHamburger(!hamburger) };
+
     return (
         <nav className="navbar">
-            <Link to="/" className="logo">&lt;/&gt; John Doe</Link>
+            <Link to="/" className="logo"><span>&lt;/&gt;</span> John Doe</Link>
 
-            <div className="navbar-links">
-                <Link to="/">Home</Link>
-                <Link to="/articles">Articles</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+            <div className={hamburger ? "navbar-links hamburger-open" : "navbar-links hamburger-closed"}>
+                <NavLink to="/" activeClassName="nav-link-highlight" onClick={ toggleHamburger } exact>Home</NavLink>
+                <NavLink to="/articles" activeClassName="nav-link-highlight" onClick={ toggleHamburger }>Articles</NavLink>
+                <NavLink to="/about" activeClassName="nav-link-highlight" onClick={ toggleHamburger }>About</NavLink>
+                <NavLink to="/contact" activeClassName="nav-link-highlight" onClick={ toggleHamburger }>Contact</NavLink>
+                <div className="hamburger-close-btn" onClick={ toggleHamburger }><i className="fas fa-times"></i></div>
             </div>
 
-            <button className="hamburger-open-btn" onClick={ openHamburger }><i className="fas fa-bars"></i></button>
-
-            <div className="hamburger-menu">
-                <div className="hamburger-left">
-                    <div className="hamburger-close-btn" onClick={ closeHamburger }><i className="fas fa-times"></i></div>
-                </div>
-                <div className="hamburger-right">
-                    <Link to="/">Home</Link>
-                    <Link to="/articles">Articles</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
-				</div>
-            </div>
+            <button className="hamburger-open-btn" onClick={ toggleHamburger }>
+                <i className="fas fa-bars"></i>
+            </button>
         </nav>
     );
 };
-
-const openHamburger = () => { document.querySelector('.hamburger-menu').style.left = "0vh"};
-const closeHamburger = () => { document.querySelector('.hamburger-menu').style.left = "-100vh"};
 
 export default Navbar;
